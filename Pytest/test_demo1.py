8b3229d239133eb905eece3555a8d9a299d9d142
Baurzhan <baurzhan@dcmsys.com>
2023-01-07 10:37:01 +0600

    use multiple in fixture

--- Pytest/test_demo1.py
+++ Pytest/test_demo1.py
@@ -1,18 +1,19 @@
 import pytest
 
+@pytest.mark.usefixtures("setup")
 class Testexample:
 
-def test_fixturedemo(setup):
-    print("Hello middle")
+    def test_fixturedemo(self):
+        print("Hello middle")
 
 
-def test_fixturedemo1(setup):
-    print("Hello middle1")
+    def test_fixturedemo1(self):
+        print("Hello middle1")
 
 
-def test_fixturedemo2(setup):
-    print("Hello middle2")
+    def test_fixturedemo2(self):
+        print("Hello middle2")
 
 
-def test_fixturedemo3(setup):
-    print("Hello middle3")
+    def test_fixturedemo3(self):
+        print("Hello middle3")
