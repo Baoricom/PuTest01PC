661bdcb767a8e10382e0f124d721b8131b06785b
Baoricom <superbaur@gmail.com>
2023-01-11 09:45:22 +0600

    added fixture dataload

--- Pytest/conftest.py
+++ Pytest/conftest.py
@@ -4,6 +4,12 @@ import pytest
 @pytest.fixture(scope="class")
 def setup():
     print("I'l be running first")
-#this part for open browser etc
+    # this part for open browser etc
     yield
     print("I'l be running last")
+
+
+@pytest.fixture()
+def dataLoad():
+    print("user profile data is being created")
+    return ["Baur", "Ibrayev", "shonay.kz"]
